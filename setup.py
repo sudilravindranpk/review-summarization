fdb0688ec8c7cf97f8c7c400c38d7d267eb25be0
PROWARENESSDC\s.ravindran <s.ravindran@prowareness.nl>
2021-02-02 12:41:22 +0530

    initial project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='review text summarization using encoder edcoder (bidirectional lstm) and attention model',
+    author='sudil ravindran pk',
+    license='',
+)
